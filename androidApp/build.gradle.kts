ba7678e7fbdeed8fdd20f04060bc6925d576a655
Avicennasis <Avicennasis@gmail.com>
2026-04-14 18:12:03 -0400

    chore: bump version to 0.9.0 for Linux BLE support

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.avicennasis.bluepaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.8.0"
+        versionCode = 2
+        versionName = "0.9.0"
     }
 
     compileOptions {
